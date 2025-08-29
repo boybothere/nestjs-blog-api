66786858e060a4adc97c6f94e9f680d4b68eedb5
Adrian Fernandes <boybothere@gmail.com>
2025-08-29 16:58:54 +0530

    add password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
     constructor(private prisma: PrismaService) { }
     async createUser(user: CreateUserDto) {
-        return await this.prisma.user.create({
-            data: {
-                ...user
-            }
+
+
+        const existingUser = await this.prisma.user.findUnique({
+            where: { username: user.username },
+        });
+
+        if (existingUser) {
+            throw new ConflictException('Username already exists');
+        }
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        const userInfo = {
+            ...user,
+            password: hashedPassword
+        }
+        const createdUser = await this.prisma.user.create({
+            data: userInfo
         })
+        const { password, ...safeUser } = createdUser;
+        return { user: safeUser }
+
     }
 }
