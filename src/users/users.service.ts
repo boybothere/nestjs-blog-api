8e3797384523a5273ca31f1a1c7a2b626ab422fe
Adrian Fernandes <boybothere@gmail.com>
2025-08-29 16:38:37 +0530

    modify tag model to insert createdAt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './user.entity';
-import { Repository } from 'typeorm';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
